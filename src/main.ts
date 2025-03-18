974c697077e2d47eafd22ee61228b3f210b8b96e
hfutgf <beretax308@gmail.com>
2025-03-18 21:29:30 +0500

    feat: politic config and swagger implementation

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,48 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.setGlobalPrefix('/api/v1');
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('Stream WebApp API')
+    .setDescription('API documentation for the Stream Web Application backend')
+    .setVersion('1.0')
+    .addBearerAuth({ type: 'http', scheme: 'bearer', bearerFormat: 'JWT' })
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api/docs', app, document, {
+    swaggerOptions: {
+      persistAuthorization: true,
+    },
+  });
+
+  app.enableCors({
+    origin: process.env.CLIENT_USER,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
+  const port = process.env.PORT;
+  await app.listen(port);
+
+  // eslint-disable-next-line no-console
+  console.log(`Application is running on: http://localhost:${port}`);
 }
+
 bootstrap();
