df056e9eea4f88322331c114ffee40a1a7f7b06f
hfutgf <beretax308@gmail.com>
2025-03-31 03:48:59 +0500

    feat: create video implementation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,23 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 import { AuthModule } from './modules/auth/auth.module';
 import { PrismaModule } from './modules/prisma/prisma.module';
 import { UserModule } from './modules/user/user.module';
+import { VideoModule } from './modules/video/video.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), PrismaModule, UserModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot(),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
+    PrismaModule,
+    UserModule,
+    AuthModule,
+    VideoModule,
+  ],
 })
 export class AppModule {}
